a37eb1d90860efd0daa08ae162c357faef1e553a
admodev <admodevcodes@outlook.com>
2024-07-20 19:57:38 -0300

    add more basic syntax and functions

--- main.dart
+++ main.dart
@@ -1,25 +1,48 @@
 void main() {
-    // Variable Types
-    String name = 'Adolfo';
+  // Variable Types
+  String name = 'Adolfo';
 
-    // Built-in functions
-    print('Hello, ${name}');
+  int age = 27;
+  int youngAge = 25;
+  int year = 2000;
+  List<String> fruits = ['Apple', 'Watermelon', 'Lemon', 'Grapes'];
 
-    int age = 27;
-    int youngAge = 25;
+  // Built-in functions
+  print('Hello, ${name}');
 
-    // Control structures
-    if (age > youngAge) {
-        print('You are growing older!');
-    }
+  // Get runtime type of variable
+  print(fruits.runtimeType);
 
-    // Iteration
-    List<String> fruits = ['Apple', 'Watermelon', 'Lemon', 'Grapes'];
+  // Control structures
+  if (age > youngAge) {
+    print('You are growing older!');
+  }
 
-    for (final fruit in fruits) {
-        print(fruit);
-    }
+  while (year < 2024) {
+    year += 1;
+    print(year);
+  }
 
-    // Get runtime type of variable
-    print(fruits.runtimeType);
+  // Iteration
+  for (final fruit in fruits) {
+    print(fruit);
+  }
+
+  for (int i = 0; i < 10; i++) {
+    print(i);
+  }
+
+  // Functions
+  int fibonacci(int n) {
+    if (n == 0 || n == 1) return n;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+  var result = fibonacci(30);
+
+  print(result);
+
+  // Shorthand (arrow) function syntax
+  fruits.where((fruitName) => fruitName.contains('mon')).forEach(print);
 }
