1a7c5c77f3a4e9010e5a5ccd9acd8df13cd04c6b
admodev <admodevcodes@outlook.com>
2024-07-20 20:11:02 -0300

    add classes

--- main.dart
+++ main.dart
@@ -9,6 +9,39 @@ import 'dart:math';
 // Importing files
 //import 'path/to/my_other_file.dart';
 
+// Classes
+class Spacecraft {
+  String? name;
+  DateTime? launchDate;
+
+  // Read-only non-final property
+  int? get launchYear => launchDate?.year;
+
+  // Constructor, with syntactic sugar for assignment to members
+  Spacecraft(this.name, this.launchDate) {
+    // Initialization code goes here.
+  }
+
+  // Named constructor that forwards to the default one.
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  // Method
+  void describe() {
+    print('Spacecraft: $name');
+
+    // Type promotion doesn't work on getters
+    var launchDate = this.launchDate;
+
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+
 void main() {
   // Variable Types
   String name = 'Adolfo';
@@ -61,4 +94,11 @@ void main() {
   var randomNumber = Random().nextInt(10).toString();
 
   print(randomNumber);
+
+  // Classes
+  var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
+  voyager.describe();
+
+  var voyager3 = Spacecraft.unlaunched('Voyager III');
+  voyager3.describe();
 }
