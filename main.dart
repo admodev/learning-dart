4887e56ec8e1baef84b3529e3238935f0436573d
admodev <admodevcodes@outlook.com>
2024-07-21 12:32:48 -0300

    add enums

--- main.dart
+++ main.dart
@@ -101,4 +101,26 @@ void main() {
 
   var voyager3 = Spacecraft.unlaunched('Voyager III');
   voyager3.describe();
+
+  // Enums
+  enum Planet {
+    mercury(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
+    venus(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
+    // ···
+    uranus(planetType: PlanetType.ice, moons: 27, hasRings: true),
+    neptune(planetType: PlanetType.ice, moons: 14, hasRings: true);
+
+    /// A constant generating constructor
+    const Planet(
+      {required this.planetType, required this.moons, required this.hasRings});
+
+    /// All instance variables are final
+    final PlanetType planetType;
+    final int moons;
+    final bool hasRings;
+
+    /// Enhanced enums support getters and other methods
+    bool get isGiant =>
+      planetType == PlanetType.gas || planetType == PlanetType.ice;
+  }
 }
